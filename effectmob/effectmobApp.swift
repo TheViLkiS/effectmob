e70e8035e1a349c4340248fd75664bba81f18a71
TheViLkiS <thevilkis@gmail.com>
2024-04-06 11:37:52 +0400

    Initial Commit

---
+++ effectmob/effectmobApp.swift
@@ -0,0 +1,17 @@
+//
+//  effectmobApp.swift
+//  effectmob
+//
+//  Created by Дмитрий Гусев on 06.04.24.
+//
+
+import SwiftUI
+
+@main
+struct effectmobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
